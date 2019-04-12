bbc817a5a228219a529d87bccd57c80ae27de30b
Leon van Bokhorst <l.vanbokhorst@fontys.nl>
2019-04-12 22:44:43 +0200

    Sharedmodel module for both REST and Websockets

--- websocketsclient/src/main/java/nl/fhict/s3/websocketclient/App.java
+++ websocketsclient/src/main/java/nl/fhict/s3/websocketclient/App.java
@@ -4,6 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         WebsocketClient websocketClient = new WebsocketClient();
+        String phoneyBeloni = "";
         websocketClient.start();
     }
 }
